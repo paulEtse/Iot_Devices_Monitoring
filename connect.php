b02212db925a7499c7c9de320e9446c72271096e
paulEtse <fiagnonpaule@gmail.com>
2020-03-19 17:37:26 +0100

    Working on connection

--- connect.php
+++ connect.php
@@ -3,19 +3,19 @@
 <body>
 
 <h1>My first PHP page</h1>
-
-<?php
-$host = 'localhost';
-// Database name
-$dbname = 'iot';
-// Username
-$sdbuser = 'paul';
-// Password
-$dbpass = 'mysqlserver';
-
-return $settings;
-$connexion = new mysqli($host,$sdbuser,$dbpass,$dbpass);
-echo "paul" ;
-?>
+<p>
+    <?php
+    $host = 'localhost';
+    // Database name
+    $dbname = 'iot';
+    // Username
+    $sdbuser = 'paul';
+    // Password
+    $dbpass = 'mysqlserver';
+    echo "paul" ;
+    $connexion = new mysqli($host,$sdbuser,$dbpass,$dbpass);
+    echo "paul" ;
+    ?>
+</p>
 </body>
 </html>
