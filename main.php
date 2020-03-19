9e42799683ed45ff4d952adaf6a4161ce4187ec9
paulEtse <fiagnonpaule@gmail.com>
2020-03-19 20:42:12 +0100

    Working on connection

--- main.php
+++ main.php
@@ -9,6 +9,9 @@
   </head>
   <body>
 <?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
 require_once 'Iot/Module.php';
 require_once 'Iot/Historique.php';
 $res =Module::getModules();
