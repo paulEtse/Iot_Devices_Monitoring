89356de7acf6d8bcaaa31d3a70f054803275f1ba
paulEtse <fiagnonpaule@gmail.com>
2020-03-19 18:29:44 +0100

    Working on connection

--- connect.php
+++ connect.php
@@ -9,7 +9,7 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    use iot\Module;
+    require './iot/Module.php';
     $host = 'localhost';
     // Database name
     $dbname = 'iot';
