0934fd562910737c78f6911763608e3003908b10
paulEtse <fiagnonpaule@gmail.com>
2020-03-19 18:39:01 +0100

    Working on connection

--- connect.php
+++ connect.php
@@ -9,7 +9,7 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    require app_path().'/iot/Module.php';
+    require '/iot/Module.php';
     $host = 'localhost';
     // Database name
     $dbname = 'iot';
