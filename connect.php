075154b708381cb7ec0ad8fc2a2697fff3269675
paulEtse <fiagnonpaule@gmail.com>
2020-03-19 17:10:43 +0100

    Working on connection

--- connect.php
+++ connect.php
@@ -7,8 +7,8 @@
 <?php
 include('dbsetting.php');
 $setting = new dbsetting().getSeting();
-$connexion = new mysqli($setting['dbhost'],$setting['dbusernamen'],$setting['dbpassword']);
-echo(con);
+$connexion = new mysqli($setting['dbhost'],$setting['dbusernamen'],$setting['dbpassword'])
+or die("unable to connect to "+$setting['dbhost']);
 echo("paul");
 ?>
 </body>
