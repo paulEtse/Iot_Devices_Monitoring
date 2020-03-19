412ebf0abef1321232ee3f835e1b5eb307ccdabc
paulEtse <fiagnonpaule@gmail.com>
2020-03-19 17:13:18 +0100

    Working on connection

--- connect.php
+++ connect.php
@@ -5,6 +5,9 @@
 <h1>My first PHP page</h1>
 
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include('dbsetting.php');
 $setting = new dbsetting().getSeting();
 $connexion = new mysqli($setting['dbhost'],$setting['dbusernamen'],$setting['dbpassword'])
