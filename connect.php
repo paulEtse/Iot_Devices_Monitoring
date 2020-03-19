95837121009c1e168683acc69066cae1b2d0f2e7
Paul Etse <paul@Pauls-MacBook-Pro.local>
2020-03-19 16:57:24 +0100

    Working on connection

--- connect.php
+++ connect.php
@@ -3,4 +3,5 @@ include('dbsetting.php');
 $setting = new dbsetting().getSeting();
 $connexion = new mysqli($setting['dbhost'],$setting['dbusernamen'],$setting['dbpassword']);
 echo(con);
+echo("paul");
 ?>
