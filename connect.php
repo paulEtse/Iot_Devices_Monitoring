0c5809980c3b67dd84e195287c4bee9c0c67a2f5
paulEtse <fiagnonpaule@gmail.com>
2020-03-19 18:58:54 +0100

    Working on connection

--- connect.php
+++ connect.php
@@ -9,7 +9,7 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    include 'module.php';
+    include_once 'Module.php';
     $host = 'localhost';
     // Database name
     $dbname = 'iot';
