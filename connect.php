b1010c6668558037d34940508e073aa6af6d0159
paulEtse <fiagnonpaule@gmail.com>
2020-03-19 18:24:02 +0100

    Working on connection

--- connect.php
+++ connect.php
@@ -4,7 +4,11 @@
 
 <h1>My first PHP page</h1>
 <p>
+    
     <?php 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     use iot\Module;
     $host = 'localhost';
     // Database name
