da2915808da42cce91c82b1eb82e1d75caa64f38
paulEtse <fiagnonpaule@gmail.com>
2020-03-22 01:08:38 +0100

    Posting data

--- script.js
+++ script.js
@@ -2,15 +2,11 @@ function submission()
 {
     var ok=true;
     var name=document.getElementById('inputName').value;
-    alert(name);
     var type=document.getElementById('inputType').value;
-    alert(type);
     var comment=document.getElementById("areaComment").value;
-    alert(comment);
     //var temperature= $("#selectionTemperature").val();
     //var time=$("#selectionTime");
     var state= $("input[name='state']:checked").val();
-    alert(state);
     const URL= "post.php";
     data={
         name:name,
